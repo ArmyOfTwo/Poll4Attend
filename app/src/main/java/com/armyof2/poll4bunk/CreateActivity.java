33a636ac67a5ceea6f45139aacbaec1830cd46db
Fx3xAxRx <fxexaxrx@gmail.com>
2018-07-13 00:56:42 +0530

    firebase server set-up

--- app/src/main/java/com/armyof2/poll4bunk/CreateActivity.java
+++ app/src/main/java/com/armyof2/poll4bunk/CreateActivity.java
@@ -4,15 +4,17 @@ package com.armyof2.poll4bunk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class CreateActivity extends AppCompatActivity{
+public class CreateActivity extends AppCompatActivity {
 
     private EditText bunkName;
     private EditText bunkDate0;
@@ -24,6 +26,17 @@ public class CreateActivity extends AppCompatActivity{
     private DatabaseReference myRef0;
     private DatabaseReference myRef1;
     private DatabaseReference myRef2;
+    private String s1 = "", s2 = "", s3 = "";
+    private int i, x;
+    private boolean next = true;
+
+    private class BunkServer {
+        String name;
+        String date;
+        int num;
+    }
+
+    BunkServer[] bunk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,21 +49,59 @@ public class CreateActivity extends AppCompatActivity{
         bunkDate2 = (EditText) findViewById(R.id.et_date2);
         bunkNum = (EditText) findViewById(R.id.et_numofparti);
         intent = new Intent(this, LaunchActivity.class);
+        bunk = new BunkServer[50];
         database = FirebaseDatabase.getInstance();
-        myRef0 = database.getReference("bname");
-        myRef1 = database.getReference("bdate");
-        myRef2 = database.getReference("bnum");
+
+        for (i = 0; i < 50; i++) {
+
+            bunk[i] = new BunkServer();
+            s1 = "bunk" + i + "name";
+            s2 = "bunk" + i + "date";
+            s3 = "bunk" + i + "num";
+
+            //Get from database
+            myRef0 = database.getReference(s1);
+            myRef1 = database.getReference(s2);
+            myRef2 = database.getReference(s3);
+
+            // Read from the database
+            myRef0.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (!next) {
+                        x = i;
+                        Log.d("TAG", "Value of x is: " + x);
+                        return;
+                    }
+                    String value = dataSnapshot.getValue(String.class);
+                    Log.d("TAG", "Value is: " + value);
+                    if(value == null) {
+                        next = false;
+                        Log.d("TAG", "next is false now");
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    Log.w("TAG", "Failed to read value.", error.toException());
+                }
+            });
+            /*if(!next) {
+                Log.d("TAG", "Break at i: " + i);
+                break;
+            }*/
+        }
+        myRef0 = database.getReference(s1);
+        myRef1 = database.getReference(s2);
+        myRef2 = database.getReference(s3);
     }
 
     public void onCreateButtonClicked(View view) {
-        String bname, bdate;
-        int bnum;
-        bname = bunkName.getText().toString();
-        bdate = (bunkDate0.getText().toString() +"-"+ bunkDate1.getText().toString() +"-"+ bunkDate2.getText().toString());
-        bnum = Integer.parseInt(bunkNum.getText().toString());
-        makeFile(bname, bdate, bnum);
-
-
+        bunk[x].name = bunkName.getText().toString();
+        bunk[x].date = (bunkDate0.getText().toString() + "-" + bunkDate1.getText().toString() + "-" + bunkDate2.getText().toString());
+        bunk[x].num = Integer.parseInt(bunkNum.getText().toString());
+        makeFile(bunk[x].name, bunk[x].date, bunk[x].num);
+        //start
         startActivity(intent);
     }
 
@@ -60,3 +111,4 @@ public class CreateActivity extends AppCompatActivity{
         myRef2.setValue(bnum);
     }
 }
+
