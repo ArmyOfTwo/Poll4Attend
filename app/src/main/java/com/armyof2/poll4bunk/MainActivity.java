d766763533995c0a3a22299fd9d6434ca74f7ab1
Fx3xAxRx <fxexaxrx@gmail.com>
2018-07-14 22:07:40 +0530

    polling works, need some logic fixes

--- app/src/main/java/com/armyof2/poll4bunk/MainActivity.java
+++ app/src/main/java/com/armyof2/poll4bunk/MainActivity.java
@@ -13,11 +13,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 import static com.armyof2.poll4bunk.LaunchActivity.SERVER_ID;
+import static com.armyof2.poll4bunk.SignInActivity.userUid;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +26,11 @@ public class MainActivity extends AppCompatActivity {
     private int option = 4;
     private String i = "0", j = "0", k = "0", l = "0";
     private FirebaseDatabase database;
-    private DatabaseReference myRef;
+    private DatabaseReference myRef, myRef2;
     private ArrayList<String> bunkServerStuff;
     private ArrayList<String> bunkServerVotes;
     private RadioButton r1, r2, r3, r4;
+    private String hasVoted = "yolo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         bunkServerVotes = new ArrayList<>();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child(SERVER_ID);
+        myRef2 = database.getReference().child(SERVER_ID).child("YuserUIDs");
 
         myRef.child("Yes");
         myRef.child("Ye");
@@ -96,40 +98,80 @@ public class MainActivity extends AppCompatActivity {
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getValue(String.class);
-                bunkServerStuff.add(value);
-                Log.d("TAG", "onChildAdded: " + bunkServerStuff);
-                dateView.setText(bunkServerStuff.get(0));
-                if (bunkServerStuff.size() == 3)
-                    titleView.setText(bunkServerStuff.get(2));
-                bunkServerVotes.add(value);
-                if (dataSnapshot.getKey().equals("Yes")) {
-                    i = value;
-                    Log.d("TAG", "i = " + i);
+                if(!dataSnapshot.getKey().equals("YuserUIDs")) {
+                    String value = dataSnapshot.getValue(String.class);
+                    bunkServerStuff.add(value);
+                    Log.d("TAG", "onChildAdded: " + bunkServerStuff);
+                    dateView.setText(bunkServerStuff.get(0));
+                    if (bunkServerStuff.size() == 3)
+                        titleView.setText(bunkServerStuff.get(2));
+                    bunkServerVotes.add(value);
+                    if (dataSnapshot.getKey().equals("Yes")) {
+                        i = value;
+                        Log.d("TAG", "i = " + i);
+                    }
+                    if (dataSnapshot.getKey().equals("Ye"))
+                        j = value;
+                    if (dataSnapshot.getKey().equals("Yes80"))
+                        k = value;
+                    if (dataSnapshot.getKey().equals("Undec"))
+                        l = value;
                 }
-                if (dataSnapshot.getKey().equals("Ye"))
-                    j = value;
-                if (dataSnapshot.getKey().equals("Yes80"))
-                    k = value;
-                if (dataSnapshot.getKey().equals("Undec"))
-                    l = value;
                     //Log.d("TAG", "i = " + i + ", j = " + j + ", k = " + k + ", l = " + l);
-                }
+            }
 
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getValue(String.class);
-                bunkServerVotes.add(value);
-                if (dataSnapshot.getKey().equals("Yes"))
-                    i = value;
-                if (dataSnapshot.getKey().equals("Ye"))
-                    j = value;
-                if (dataSnapshot.getKey().equals("Yes80"))
-                    k = value;
-                if (dataSnapshot.getKey().equals("Undec"))
-                    l = value;
+                if(!dataSnapshot.getKey().equals("YuserUIDs")) {
+                    String value = dataSnapshot.getValue(String.class);
+                    bunkServerVotes.add(value);
+                    if (dataSnapshot.getKey().equals("Yes"))
+                        i = value;
+                    if (dataSnapshot.getKey().equals("Ye"))
+                        j = value;
+                    if (dataSnapshot.getKey().equals("Yes80"))
+                        k = value;
+                    if (dataSnapshot.getKey().equals("Undec"))
+                        l = value;
                     Log.d("TAG", "i = " + i + ", j = " + j + ", k = " + k + ", l = " + l);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        myRef2.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                if (dataSnapshot.getKey().equals(userUid)) {
+                    hasVoted = value;
+                    Log.d("TAG", "onChildAdded : hasVoted = " + hasVoted);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                if (dataSnapshot.child("YuserUIDs").getKey().equals(userUid)) {
+                    hasVoted = value;
+                    Log.d("TAG", "onChildChanged : hasVoted = " + hasVoted);
+                }
             }
 
             @Override
@@ -154,25 +196,97 @@ public class MainActivity extends AppCompatActivity {
             case 1:
                 int p = Integer.parseInt(i);
                 ++p;
+                int q = Integer.parseInt(j);
+                --q;
+                int r = Integer.parseInt(k);
+                --r;
+                int s = Integer.parseInt(l);
+                --s;
+
+                if(hasVoted.equals("yes")) {
+                    break;
+                } else if(hasVoted.equals("no")){
+                    myRef.child("Ye").setValue(Integer.toString(q));
+                } else if(hasVoted.equals("yes80")){
+                    myRef.child("Yes80").setValue(Integer.toString(r));
+                } else if(hasVoted.equals("undec")){
+                    myRef.child("Undec").setValue(Integer.toString(s));
+                }
                 myRef.child("Yes").setValue(Integer.toString(p));
+                myRef.child("YuserUIDs").child(userUid).setValue("yes");
+                hasVoted = "yes";
                 break;
 
             case 2:
-                int q = Integer.parseInt(j);
+                p = Integer.parseInt(i);
+                --p;
+                q = Integer.parseInt(j);
                 ++q;
+                r = Integer.parseInt(k);
+                --r;
+                s = Integer.parseInt(l);
+                --s;
+
+                if(hasVoted.equals("no")) {
+                    break;
+                } else if(hasVoted.equals("yes")){
+                    myRef.child("Yes").setValue(Integer.toString(p));
+                } else if(hasVoted.equals("yes80")){
+                    myRef.child("Yes80").setValue(Integer.toString(r));
+                } else if(hasVoted.equals("undec")){
+                    myRef.child("Undec").setValue(Integer.toString(s));
+                }
                 myRef.child("Ye").setValue(Integer.toString(q));
+                myRef.child("YuserUIDs").child(userUid).setValue("no");
+                hasVoted = "no";
                 break;
 
             case 3:
-                int r = Integer.parseInt(k);
+                p = Integer.parseInt(i);
+                --p;
+                q = Integer.parseInt(j);
+                --q;
+                r = Integer.parseInt(k);
                 ++r;
+                s = Integer.parseInt(l);
+                --s;
+
+                if(hasVoted.equals("yes80")) {
+                    break;
+                } else if(hasVoted.equals("no")){
+                    myRef.child("Ye").setValue(Integer.toString(q));
+                } else if(hasVoted.equals("yes")){
+                    myRef.child("Yes").setValue(Integer.toString(p));
+                } else if(hasVoted.equals("undec")){
+                    myRef.child("Undec").setValue(Integer.toString(s));
+                }
                 myRef.child("Yes80").setValue(Integer.toString(r));
+                myRef.child("YuserUIDs").child(userUid).setValue("yes80");
+                hasVoted = "yes80";
                 break;
 
             case 4:
-                int s = Integer.parseInt(l);
+                p = Integer.parseInt(i);
+                --p;
+                q = Integer.parseInt(j);
+                --q;
+                r = Integer.parseInt(k);
+                --r;
+                s = Integer.parseInt(l);
                 ++s;
+
+                if(hasVoted.equals("undec")) {
+                    break;
+                } else if(hasVoted.equals("no")){
+                    myRef.child("Ye").setValue(Integer.toString(q));
+                } else if(hasVoted.equals("yes80")){
+                    myRef.child("Yes80").setValue(Integer.toString(r));
+                } else if(hasVoted.equals("yes")){
+                    myRef.child("Yes").setValue(Integer.toString(p));
+                }
                 myRef.child("Undec").setValue(Integer.toString(s));
+                myRef.child("YuserUIDs").child(userUid).setValue("undec");
+                hasVoted = "undec";
                 break;
         }
     }
